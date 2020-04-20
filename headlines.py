89d5791d12af6c9425e121c02fca147b4d4082d1
giannicole <gianni.lap@live.it>
2020-04-20 00:02:54 +0000

    headlines

---
+++ headlines.py
@@ -0,0 +1,26 @@
+import feedparser
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+RSS_FEEDS = {'bbc': "http://feeds.bbci.co.uk/news/rss.xml",
+            'cnn': 'http://rss.cnn.com/rss/edition.rss',
+            'fox':'http://feeds.foxnews.com/foxnews/latest',
+            'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+@app.route("/")
+
+
+  
+@app.route("/<pubblication>")
+def get_news(pubblication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[pubblication])
+    first_article = feed['entries'][0]
+    #passing dunamic data to my tempalte
+    return render_template("index.html", 
+            title=first_article.get("title"),
+            pubblished=first_article.get("pubblished"),
+            summary=first_article.get("summary")
+        )
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
